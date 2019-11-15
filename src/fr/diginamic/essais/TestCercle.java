53a5369413884355e40d158de4ca27088cede8ff
Matt34000 <matthieu-le-roy@gmx.fr>
2019-11-15 21:39:10 +0100

    5eme commit

---
+++ src/fr/diginamic/essais/TestCercle.java
@@ -0,0 +1,28 @@
+package fr.diginamic.essais;
+
+import fr.diginamic.entites.Cercle;
+
+public class TestCercle {
+
+	public static void main(String[] args) {
+		
+		
+		Cercle cercle1 = new Cercle(5);
+		System.out.println(cercle1);
+		System.out.println("Perimetre : " + cercle1.calcPerimetre());
+		System.out.println("Surface : " + cercle1.calcSurface());
+		
+		Cercle cercle2 = new Cercle(7);
+		System.out.println(cercle2);
+		System.out.println("Perimetre : " + cercle2.calcPerimetre());
+		System.out.println("Surface : " + cercle2.calcSurface());
+
+		Cercle cercle3 = new Cercle(75);
+		System.out.println(cercle3);
+		System.out.println("Perimetre : " + cercle3.calcPerimetre());
+		System.out.println("Surface : " + cercle3.calcSurface());
+		
+
+	}
+
+}

d961781616e43b4655fa1b6e5a2f732a42f8676e
Matt34000 <matthieu-le-roy@gmx.fr>
2019-11-15 19:14:37 +0100

    3eme commit

--- src/fr/diginamic/entites/Cercle.java
+++ src/fr/diginamic/entites/Cercle.java
@@ -2,7 +2,26 @@ package fr.diginamic.entites;
 
 public class Cercle {
 
+	// variables
+	private double rayon = 0;
+
+	// Constructor
+	public Cercle(int ray) {
+		this.rayon = ray;
+	}
 	
-	private double rayon;
+	// methode perimetre
+	public double perimetre() { 
+		double perimetre = 2 * Math.PI * rayon;
+		return perimetre;
+	}
+
+	// methode surface
+	public double surface() { 
+		double surface = Math.PI * (rayon * rayon);
+		return surface;
+	}
+
+
 	
 }

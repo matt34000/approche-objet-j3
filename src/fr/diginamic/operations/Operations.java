6f9479cf4314b5ffad42caca8a30a6070a12796d
Matt34000 <matthieu-le-roy@gmx.fr>
2019-11-15 22:22:50 +0100

    7eme commit

---
+++ src/fr/diginamic/operations/Operations.java
@@ -0,0 +1,26 @@
+package fr.diginamic.operations;
+
+public class Operations {
+
+	public static double calcul (double a, double b, char c) {
+		
+		double d = 0;
+		
+		if (c == '+') { 
+			d = a + b;			
+		}
+		else if (c == '-') {
+			d = a - b;				
+		}
+		else if (c == '*') {
+			d = a * b;				
+		}
+		else if (c == '/') {
+			d = a / b;				
+		}
+		
+		return d;
+	
+	}
+		
+}

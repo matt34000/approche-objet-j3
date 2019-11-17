4933081b925ee486a824215bb2b314588c716bb3
Matt34000 <matthieu-le-roy@gmx.fr>
2019-11-17 23:07:19 +0100

    9eme commit

--- src/fr/diginamic/essais/TestOperations.java
+++ src/fr/diginamic/essais/TestOperations.java
@@ -7,13 +7,10 @@ public class TestOperations {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("op1 : " + Operations.calcul(5.99,10.01,'+')); // 16
-		
-		System.out.println("op2 : " + Operations.calcul(24.53,7.53,'-')); // 17
-		
-		System.out.println("op3 : " + Operations.calcul(2.4,7.5,'*')); // 18
-		
-		System.out.println("op4 : " + Operations.calcul(95,5,'/')); // 19
+		System.out.println("op1 : " + Operations.calcul(5.99,10.61,'+')); 		
+		System.out.println("op2 : " + Operations.calcul(24.53,6.83,'-')); 		
+		System.out.println("op3 : " + Operations.calcul(2.35,8,'*')); 		
+		System.out.println("op4 : " + Operations.calcul(378.1,19,'/'));
 		
 		
 	}

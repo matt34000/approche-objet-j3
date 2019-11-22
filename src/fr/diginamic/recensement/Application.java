79afbf1dcc7d96517c8c6ff75d0c869d22f71fe3
Matt34000 <matthieu-le-roy@gmx.fr>
2019-11-22 10:35:35 +0100

    22eme commit

--- src/fr/diginamic/recensement/Application.java
+++ src/fr/diginamic/recensement/Application.java
@@ -1,10 +1,31 @@
 package fr.diginamic.recensement;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import org.apache.commons.io.FileUtils;
+
+
 public class Application {
-
 	public static void main(String[] args) {
+		try {
+			File file = new File("E:/diginamic/_sts/191119/recensement population 2016.csv");
+			List<String> lignes = FileUtils.readLines(file, "UTF-8");
+			for (String ligne : lignes) {
+				System.out.println(ligne);
+			}
+			System.out.println("Nombre de lignes :" + lignes.size());
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
 		
-
+		
+		
+		//String filePath = ClassLoader.getSystemClassLoader().getResource("recensement.csv").getFile();
+		//System.out.println(filePath);
+		//Recensement recensement = RecensementUtils.lire(filePath);
+		
+		
+		
 	}
-
 }
